4341e9dec30edff44a9d1eb3df96067395a82410
Antanelli <69147960+Ani-Brsoyan@users.noreply.github.com>
2024-08-13 13:58:57 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,34 @@
 from flask import Flask, jsonify, send_from_directory
+import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/data')
 def get_data():
-    # This function returns the JSON data from the file
-    with open('data.json') as f:
-        data = f.read()
-    return data
+    json_directory = '/home/ani/PlayFiles/JSandMore/multiJsonSimServer'  # Replace with your directory path
+    data = []
+
+    for file in os.listdir(json_directory):
+        if file.endswith('.json'):
+            file_path = os.path.join(json_directory, file)
+            try:
+                with open(file_path) as f:
+                    file_data = json.load(f)
+                    data.extend(file_data)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON from file {file}: {e}")
+            except Exception as e:
+                print(f"Error reading file {file}: {e}")
+
+    return jsonify(data)
 
 @app.route('/')
 def serve_index():
-    # This function serves the index.html file
     return send_from_directory('.', 'index.html')
 
 @app.route('/<path:path>')
 def serve_static(path):
-    # This function serves any other static files like CSS
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
